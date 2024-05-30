986c966eb8be04e767a8390a7c148e9e57c0cbce
smit prajapati <smitprajapati002@gmail.com>
2024-05-30 12:26:40 -0600

    solution 4 day1

---
+++ simple_interest.java
@@ -0,0 +1,12 @@
+public class simple_interest {
+
+    public static void main(String [] args){
+
+        float p = 10000, r = 5, t = 5;
+        float SI = (p * r * t) / 100;
+
+        System.out.println("Simple interest = " + SI);
+
+    }
+    
+}
