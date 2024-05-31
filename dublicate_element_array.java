d5d9f71c20f2141e089f11514ea4c744e41b3df6
smit prajapati <smitprajapati002@gmail.com>
2024-05-31 14:11:13 -0600

    hollow rectangle, solution 2 day2

--- dublicate_element_array.java
+++ dublicate_element_array.java
@@ -1,17 +1,34 @@
-public class dublicate_element_array {
-    static int removeDublicteElement(int arr[] , int n){
-        if(n==0 || n==1)
-            return n;
-        int[] temp = new int[n];
-        int j = 0;
-        for(int i = 0; i < n - 1; i++){
-            if(arr[i]!= arr[i + 1]){
-                temp[j++] = arr[i];
-            }
-        }
-    }
-    public static void main(String [] args){
+// public class dublicate_element_array {
+//     static int removeDublicteElement(int arr[] , int n){
+//         if(n==0 || n==1)
+//             return n;
+//         int[] temp = new int[n];
+//         int j = 0;
+//         for(int i = 0; i < n - 1; i++)
+//             if(arr[i]!= arr[i + 1])
+//                 temp[j++] = arr[i];
+            
+//             temp[j++] = arr[n- 1];
+            
+//         for(int p = 0 ;p < j; p++)
+//             arr[p] = temp[p];
+//         return j;
 
-    }
+        
+        
+//     }
+//     public static void main(String [] args){
+//         int arr[] = {1,2,3,5,64,3,2,1};
+//         int am = arr.length;
+//         am = removeDublicteElement(arr, am);
+//         for(int i = 0; i < am;i++){
+//             System.out.println(arr[i] + " ");
+//         }
+//     }
     
+// }
+public class dublicate_element_array{
+    public static void main(String []args){
+        
+    }
 }
