bfa09be3548e2974e7459150d424ce129481abff
smit prajapati <smitprajapati002@gmail.com>
2024-05-31 16:53:57 -0600

    solution 3 day2, Some star patterns

--- finding_large.java
+++ finding_large.java
@@ -1,12 +1,24 @@
-import java.util.Arrays;
+// import java.util.Arrays;
 
 public class finding_large {
+    static int arr[] = { 12, 51, 561, 542, 16 };
+
+    static int largest() {
+        int i;
+        int max = arr[0];
+        for (i = 1; i < arr.length; i++)
+            if (arr[i] > max)
+                max = arr[i];
+        return max;
+    }
 
     public static void main(String[] args) {
-        int arr [] = {12,51,16,15,61,15};
-        int max = Arrays.stream(arr).max().getAsInt();
+        // int arr [] = {12,51,16,15,61,15};
+        // int max = Arrays.stream(arr).max().getAsInt();
 
-        System.out.println("The maximum number in array is: "+ max);
+        // System.out.println("The maximum number in array is: "+ max);
+        System.out.println("The largest number in array is : " + largest());
+
     }
 
 }
