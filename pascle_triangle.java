7c0b629805b9be794c447b9bb9f1839f53d897e5
smit prajapati <smitprajapati002@gmail.com>
2024-05-30 19:44:21 -0600

    solution 5 day1

---
+++ pascle_triangle.java
@@ -0,0 +1,24 @@
+// public class pascle_triangle{
+//     public static void main(String [] args){
+//         int n =5;
+//         for(int i = 0; i<n;i++){
+//             for(int j = 0;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+public class pascle_triangle{
+    public static void main(String [] args){
+        int n = 5;
+        for(int i = 0;i<=n;i++){
+            for(int j = 0; j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
