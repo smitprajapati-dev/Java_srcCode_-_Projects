30e8e7ced1b7b63a1f028586ba454f0519d2adf2
smit prajapati <smitprajapati002@gmail.com>
2024-05-30 19:47:58 -0600

    solution 6 day1

---
+++ array_sum.java
@@ -0,0 +1,12 @@
+public class array_sum {
+
+    public static void main(String [] args){
+        int arr[] = {1,2,3,4,5,6};
+        int sum = 0;
+        for(int i =0;i<=arr.length;i++){
+            sum += i;
+        }
+        System.out.println("The sum of array is : " + sum);
+    }
+    
+}
