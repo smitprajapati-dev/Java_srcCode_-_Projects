3765fdf64d0a2eeacef2aa7784f6118323f349b1
smit prajapati <smitprajapati002@gmail.com>
2024-05-31 19:11:22 -0600

    solution 4 day2, star pattern 1

--- star_pattern.java
+++ star_pattern.java
@@ -57,10 +57,41 @@ public class star_pattern {
         //     }
         //     System.out.println();
         // }
+        // int n = 5;
+        // for(int i = 1; i<=n;i++){
+        //     for(int j = 1;j<=i;j++){
+        //         System.out.print(j+ " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 5;
+        // for(int i = n;i>=1;i--){
+        //     for(int j = 1;j<=i;j++){
+        //         System.out.print(j+ " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int n = 5;
+        // int number = 1;
+        // for(int i =0;i<=n;i++){
+        //     for(int j = 1;j<=i;j++){
+        //         System.out.print(number+ " ");
+        //         number++;
+        //     }
+        //     System.out.println();
+        // }
         int n = 5;
-        for(int i = 1; i<=n;i++){
+        for(int i = 1;i<=n;i++){
             for(int j = 1;j<=i;j++){
-                System.out.print(j);
+                int sum = i + j;
+                if(sum % 2 == 0){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
             }
             System.out.println();
         }
