93c1fa71a3f35e070d398b112943f413a502ad1e
smit prajapati <smitprajapati002@gmail.com>
2024-06-02 19:13:22 -0600

    solution 2 day 4, advanced star pattern

--- advance_star_pattern.java
+++ advance_star_pattern.java
@@ -2,14 +2,32 @@ public class advance_star_pattern {
 
     public static void main(String[] args) {
 
+        // int n = 5;
+        // for(int i = 1;i<=n;i++){
+        // for(int j = 1;j<=(n - i);j++){
+        // System.out.print(" ");
+        // }
+
+        // for(int j = 1;j<=5;j++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
         int n = 5;
-        for(int i = 1;i<=n;i++){
-            for(int j = 1;j<=(n - i);j++){
+        int m = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
                 System.out.print(" ");
             }
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("*");
 
-            for(int j = 1;j<=5;j++){
-                System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
